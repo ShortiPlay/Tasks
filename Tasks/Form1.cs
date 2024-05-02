9bfdbbd8d7546c620006fad1b51cdc9fb0d72422
Masha <mariaselehina11@gmail.com>
2024-05-02 21:00:36 +0300

    Написала комменатрии

--- Tasks/Form1.cs
+++ Tasks/Form1.cs
@@ -6,49 +6,65 @@ namespace Tasks
         {
             InitializeComponent();
         }
-
+        //Кнопка добавления в ListView у задач
         private void button1_Click(object sender, EventArgs e)
         {
+            //Проверка введённой информации во всех используемых полях
             if (string.IsNullOrEmpty(textBox2.Text) || string.IsNullOrEmpty(comboBox1.Text) || string.IsNullOrEmpty(comboBox2.Text) || string.IsNullOrEmpty(textBox1.Text) || string.IsNullOrEmpty(comboBox3.Text))
             {
+                //Вывод сообщения, если условие выполняется
                 MessageBox.Show("Введите значение в текстовое поле перед добавлением.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
             {
+                //Вызов метода добавления всех элементов в список
                 add(textBox2.Text, comboBox1.Text, comboBox2.Text, textBox1.Text, comboBox3.Text);
+                //Очистка TextBox
                 textBox2.Clear();
+                //Очистка ComboBox
                 comboBox1.SelectedIndex = -1;
+                //Очистка ComboBox
                 comboBox2.SelectedIndex = -1;
+                //Очистка TextBox
                 textBox1.Clear();
+                //Очистка ComboBox
                 comboBox3.SelectedIndex = -1;
             }
         }
-
+        //Кнопка добавления в ListView у сотрудников
         private void button3_Click(object sender, EventArgs e)
         {
+            //Проверка введённой информации во всех используемых полях
             if (string.IsNullOrEmpty(textBox4.Text) || string.IsNullOrEmpty(textBox3.Text) || string.IsNullOrEmpty(comboBox4.Text))
             {
+                //Вывод сообщения, если условие выполняется
                 MessageBox.Show("Введите значение в текстовое поле перед добавлением.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             else
             {
+                //Вызов метода добавления всех элементов в список
                 addColoborators(textBox4.Text, textBox3.Text, comboBox4.Text);
+                //Очистка TextBox
                 textBox4.Clear();
+                //Очистка TextBox
                 textBox3.Clear();
+                //Очистка ComboBox
                 comboBox4.SelectedIndex = -1;
             }
         }
-
+        //Кнопка удаление элементов из списка задач
         private void button2_Click(object sender, EventArgs e)
         {
+            //цикл для удаления элементов из списка
             foreach (ListViewItem item in listView1.SelectedItems)
             {
                 listView1.Items.Remove(item);
             }
         }
-
+        //Кнопка удаление элементов из списка сотрудников
         private void button4_Click(object sender, EventArgs e)
         {
+            //цикл для удаления элементов из списка
             foreach (ListViewItem item in listView2.SelectedItems)
             {
                 listView2.Items.Remove(item);
@@ -59,12 +75,14 @@ namespace Tasks
         {
 
         }
+        //Метод добавления элеметнов в список задач
         private void add(string titel, string type, string status, string term, string urgency)
         {
             string[] row = {  titel, type, status, term, urgency };
             ListViewItem item = new ListViewItem(row);
             listView1.Items.Add(item);
         }
+        //Метод добавления элементов в список сотрудников
         private void addColoborators(string employee, string missia, string post)
         {
             string[] rows = { employee, missia, post };

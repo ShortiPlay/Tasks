cb5ba55c5f41937a5459a1e04d5371cab98a4250
Masha <mariaselehina11@gmail.com>
2024-05-11 16:53:06 +0300

    изменение шрифта у tabcontrol

--- Tasks/Form1.Designer.cs
+++ Tasks/Form1.Designer.cs
@@ -30,19 +30,28 @@
         {
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            label14 = new Label();
+            richTextBox1 = new RichTextBox();
+            panel2 = new Panel();
+            label13 = new Label();
+            button5 = new Button();
+            label12 = new Label();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            label11 = new Label();
             panel1 = new Panel();
             comboBox1 = new ComboBox();
-            button1 = new Button();
-            button2 = new Button();
-            label1 = new Label();
-            label2 = new Label();
-            textBox1 = new TextBox();
-            comboBox2 = new ComboBox();
-            textBox2 = new TextBox();
-            label3 = new Label();
             comboBox3 = new ComboBox();
+            button1 = new Button();
             label4 = new Label();
             label5 = new Label();
+            button2 = new Button();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            comboBox2 = new ComboBox();
+            textBox1 = new TextBox();
+            label2 = new Label();
             label10 = new Label();
             comboBox6 = new ComboBox();
             label9 = new Label();
@@ -66,26 +75,18 @@
             employee = new ColumnHeader();
             missia = new ColumnHeader();
             post = new ColumnHeader();
-            panel2 = new Panel();
-            textBox5 = new TextBox();
-            label11 = new Label();
-            label12 = new Label();
-            textBox6 = new TextBox();
-            button5 = new Button();
-            label13 = new Label();
-            richTextBox1 = new RichTextBox();
-            label14 = new Label();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
+            panel2.SuspendLayout();
             panel1.SuspendLayout();
             tabPage2.SuspendLayout();
-            panel2.SuspendLayout();
             SuspendLayout();
             // 
             // tabControl1
             // 
             tabControl1.Controls.Add(tabPage1);
             tabControl1.Controls.Add(tabPage2);
+            tabControl1.Font = new Font("Times New Roman", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 204);
             tabControl1.Location = new Point(-6, 1);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
@@ -105,12 +106,98 @@
             tabPage1.Controls.Add(comboBox5);
             tabPage1.Controls.Add(listView1);
             tabPage1.ForeColor = Color.FromArgb(0, 0, 64);
-            tabPage1.Location = new Point(4, 29);
+            tabPage1.Location = new Point(4, 28);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(1126, 537);
+            tabPage1.Size = new Size(1126, 538);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Задачи";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label14.Location = new Point(936, 225);
+            label14.Name = "label14";
+            label14.Size = new Size(88, 23);
+            label14.TabIndex = 25;
+            label14.Text = "Заметки";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(831, 261);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(289, 270);
+            richTextBox1.TabIndex = 24;
+            richTextBox1.Text = "";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label13);
+            panel2.Controls.Add(button5);
+            panel2.Controls.Add(label12);
+            panel2.Controls.Add(textBox6);
+            panel2.Controls.Add(textBox5);
+            panel2.Controls.Add(label11);
+            panel2.Location = new Point(513, 254);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(287, 277);
+            panel2.TabIndex = 23;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label13.Location = new Point(101, 5);
+            label13.Name = "label13";
+            label13.Size = new Size(100, 23);
+            label13.TabIndex = 29;
+            label13.Text = "Изменить";
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(134, 207, 250);
+            button5.Font = new Font("Times New Roman", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button5.Location = new Point(90, 207);
+            button5.Name = "button5";
+            button5.Size = new Size(105, 49);
+            button5.TabIndex = 28;
+            button5.Text = "Изменить";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label12.Location = new Point(72, 113);
+            label12.Name = "label12";
+            label12.Size = new Size(162, 22);
+            label12.TabIndex = 26;
+            label12.Text = "Срок выполнения";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(45, 137);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(200, 27);
+            textBox6.TabIndex = 27;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(45, 73);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(200, 27);
+            textBox5.TabIndex = 25;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label11.Location = new Point(109, 50);
+            label11.Name = "label11";
+            label11.Size = new Size(90, 22);
+            label11.TabIndex = 24;
+            label11.Text = "Название";
             // 
             // panel1
             // 
@@ -139,8 +226,18 @@
             comboBox1.Items.AddRange(new object[] { "Важныве и долгосрочные", "Текукщие важные", "Повседневные", "Ситуатывные" });
             comboBox1.Location = new Point(4, 99);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(192, 28);
+            comboBox1.Size = new Size(192, 27);
             comboBox1.TabIndex = 8;
+            // 
+            // comboBox3
+            // 
+            comboBox3.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Items.AddRange(new object[] { "Не важные и срочные", "Важные и срочные", "Не срочные и не важные", "Важные но не срочные" });
+            comboBox3.Location = new Point(112, 167);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(192, 27);
+            comboBox3.TabIndex = 15;
             // 
             // button1
             // 
@@ -153,82 +250,6 @@
             button1.Text = "Добавить";
             button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
-            // 
-            // button2
-            // 
-            button2.BackColor = Color.FromArgb(134, 207, 250);
-            button2.Font = new Font("Times New Roman", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            button2.Location = new Point(282, 224);
-            button2.Name = "button2";
-            button2.Size = new Size(105, 49);
-            button2.TabIndex = 2;
-            button2.Text = "Удалить";
-            button2.UseVisualStyleBackColor = false;
-            button2.Click += button2_Click;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label1.Location = new Point(59, 5);
-            label1.Name = "label1";
-            label1.Size = new Size(90, 22);
-            label1.TabIndex = 7;
-            label1.Text = "Название";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label2.Location = new Point(53, 73);
-            label2.Name = "label2";
-            label2.Size = new Size(105, 22);
-            label2.TabIndex = 9;
-            label2.Text = "Тип задачи";
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(228, 30);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(192, 27);
-            textBox1.TabIndex = 17;
-            // 
-            // comboBox2
-            // 
-            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
-            comboBox2.FormattingEnabled = true;
-            comboBox2.Items.AddRange(new object[] { "В процессе выполнения", "Выполнена", "Отменена", "Запланирована" });
-            comboBox2.Location = new Point(228, 99);
-            comboBox2.Name = "comboBox2";
-            comboBox2.Size = new Size(192, 28);
-            comboBox2.TabIndex = 10;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(4, 30);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(200, 27);
-            textBox2.TabIndex = 16;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label3.Location = new Point(259, 76);
-            label3.Name = "label3";
-            label3.Size = new Size(128, 22);
-            label3.TabIndex = 11;
-            label3.Text = "Статус задачи";
-            // 
-            // comboBox3
-            // 
-            comboBox3.DropDownStyle = ComboBoxStyle.DropDownList;
-            comboBox3.FormattingEnabled = true;
-            comboBox3.Items.AddRange(new object[] { "Не важные и срочные", "Важные и срочные", "Не срочные и не важные", "Важные но не срочные" });
-            comboBox3.Location = new Point(112, 167);
-            comboBox3.Name = "comboBox3";
-            comboBox3.Size = new Size(192, 28);
-            comboBox3.TabIndex = 15;
             // 
             // label4
             // 
@@ -250,6 +271,72 @@
             label5.TabIndex = 14;
             label5.Text = "Срочность задачи";
             // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(134, 207, 250);
+            button2.Font = new Font("Times New Roman", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            button2.Location = new Point(282, 224);
+            button2.Name = "button2";
+            button2.Size = new Size(105, 49);
+            button2.TabIndex = 2;
+            button2.Text = "Удалить";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label3.Location = new Point(259, 76);
+            label3.Name = "label3";
+            label3.Size = new Size(128, 22);
+            label3.TabIndex = 11;
+            label3.Text = "Статус задачи";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(4, 30);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(200, 27);
+            textBox2.TabIndex = 16;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(59, 5);
+            label1.Name = "label1";
+            label1.Size = new Size(90, 22);
+            label1.TabIndex = 7;
+            label1.Text = "Название";
+            // 
+            // comboBox2
+            // 
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "В процессе выполнения", "Выполнена", "Отменена", "Запланирована" });
+            comboBox2.Location = new Point(228, 99);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(192, 27);
+            comboBox2.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(228, 30);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(192, 27);
+            textBox1.TabIndex = 17;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.Location = new Point(53, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(105, 22);
+            label2.TabIndex = 9;
+            label2.Text = "Тип задачи";
+            // 
             // label10
             // 
             label10.AutoSize = true;
@@ -266,7 +353,7 @@
             comboBox6.Items.AddRange(new object[] { "В процессе выполнения", "Выполнена", "Отменена", "Запланирована" });
             comboBox6.Location = new Point(870, 104);
             comboBox6.Name = "comboBox6";
-            comboBox6.Size = new Size(227, 28);
+            comboBox6.Size = new Size(227, 27);
             comboBox6.TabIndex = 20;
             // 
             // label9
@@ -284,7 +371,7 @@
             comboBox5.Items.AddRange(new object[] { "Не важные и срочные", "Важные и срочные", "Не срочные и не важные", "Важные но не срочные" });
             comboBox5.Location = new Point(870, 29);
             comboBox5.Name = "comboBox5";
-            comboBox5.Size = new Size(227, 28);
+            comboBox5.Size = new Size(227, 27);
             comboBox5.TabIndex = 18;
             // 
             // listView1
@@ -356,7 +443,7 @@
             comboBox4.Items.AddRange(new object[] { "Программист", "Менеджер", "Дизайнер", "Тестировщик", "Team lead", "Системный адиминстратор" });
             comboBox4.Location = new Point(75, 204);
             comboBox4.Name = "comboBox4";
-            comboBox4.Size = new Size(245, 28);
+            comboBox4.Size = new Size(245, 27);
             comboBox4.TabIndex = 13;
             // 
             // label6
@@ -465,92 +552,6 @@
             post.TextAlign = HorizontalAlignment.Center;
             post.Width = 200;
             // 
-            // panel2
-            // 
-            panel2.Controls.Add(label13);
-            panel2.Controls.Add(button5);
-            panel2.Controls.Add(label12);
-            panel2.Controls.Add(textBox6);
-            panel2.Controls.Add(textBox5);
-            panel2.Controls.Add(label11);
-            panel2.Location = new Point(513, 254);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(287, 277);
-            panel2.TabIndex = 23;
-            // 
-            // textBox5
-            // 
-            textBox5.Location = new Point(45, 73);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(200, 27);
-            textBox5.TabIndex = 25;
-            // 
-            // label11
-            // 
-            label11.AutoSize = true;
-            label11.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label11.Location = new Point(109, 50);
-            label11.Name = "label11";
-            label11.Size = new Size(90, 22);
-            label11.TabIndex = 24;
-            label11.Text = "Название";
-            // 
-            // label12
-            // 
-            label12.AutoSize = true;
-            label12.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label12.Location = new Point(72, 113);
-            label12.Name = "label12";
-            label12.Size = new Size(162, 22);
-            label12.TabIndex = 26;
-            label12.Text = "Срок выполнения";
-            // 
-            // textBox6
-            // 
-            textBox6.Location = new Point(45, 137);
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(200, 27);
-            textBox6.TabIndex = 27;
-            // 
-            // button5
-            // 
-            button5.BackColor = Color.FromArgb(134, 207, 250);
-            button5.Font = new Font("Times New Roman", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            button5.Location = new Point(90, 207);
-            button5.Name = "button5";
-            button5.Size = new Size(105, 49);
-            button5.TabIndex = 28;
-            button5.Text = "Изменить";
-            button5.UseVisualStyleBackColor = false;
-            // 
-            // label13
-            // 
-            label13.AutoSize = true;
-            label13.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            label13.Location = new Point(101, 5);
-            label13.Name = "label13";
-            label13.Size = new Size(100, 23);
-            label13.TabIndex = 29;
-            label13.Text = "Изменить";
-            // 
-            // richTextBox1
-            // 
-            richTextBox1.Location = new Point(831, 261);
-            richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(289, 270);
-            richTextBox1.TabIndex = 24;
-            richTextBox1.Text = "";
-            // 
-            // label14
-            // 
-            label14.AutoSize = true;
-            label14.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            label14.Location = new Point(936, 225);
-            label14.Name = "label14";
-            label14.Size = new Size(88, 23);
-            label14.TabIndex = 25;
-            label14.Text = "Заметки";
-            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
@@ -564,12 +565,12 @@
             tabControl1.ResumeLayout(false);
             tabPage1.ResumeLayout(false);
             tabPage1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             tabPage2.ResumeLayout(false);
             tabPage2.PerformLayout();
-            panel2.ResumeLayout(false);
-            panel2.PerformLayout();
             ResumeLayout(false);
         }
 
